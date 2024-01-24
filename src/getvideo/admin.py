a5c7a737ca808d8701635c280fc94b3d4c50a7f4
adivis <adi.vishwakarma01@gmail.com>
2024-01-24 18:43:01 +0530

    feat: dashboard to view the stored videos with filters and sorting options

--- src/getvideo/admin.py
+++ src/getvideo/admin.py
@@ -1,5 +1,15 @@
+from django.contrib.admin import ModelAdmin
 from django.contrib import admin
 from getvideo.models import Video
 # Register your models here.
 
-admin.site.register(Video)
+class VideoAdmin(ModelAdmin):
+    
+    list_display = ('videoId','title','published_at')
+    search_fields = ('videoId','title','description')
+    ordering = ['published_at']
+    list_filter = ['published_at']
+    
+admin.site.register(Video, VideoAdmin)
+
+    
