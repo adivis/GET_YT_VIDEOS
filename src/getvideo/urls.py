c993dd4c8e003891e192cea7466aa2542b3ba0cc
adivis <adi.vishwakarma01@gmail.com>
2024-01-24 10:29:27 +0530

    feat: get videos stored in DB sorted wrt pushblished_at

---
+++ src/getvideo/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+from getvideo.views import get_videos
+
+urlpatterns = [
+    path('', get_videos)
+]
--- src/getvideo/views.py
+++ src/getvideo/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 
+from getvideo.models import Video
 # Create your views here.
+
+
+def get_videos(request):
+    """returns the stored video data in a response sorted in descending order of published datetime """
+    if request.method != 'GET':
+        return JsonResponse({"error":"Method Not Allowed"}, status=405)
+    
+    # get sorted in descending order ('-' is used as NOT)
+    videos = Video.objects.all().order_by('-published_at').values()
+
+    return JsonResponse(list(videos), safe=False)
