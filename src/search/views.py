e8cf70278ac52475e9302e4f307d48a279134155
adivis <adi.vishwakarma01@gmail.com>
2024-01-24 13:38:16 +0530

    feat: search api to get video based on title and description given

--- src/search/views.py
+++ src/search/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
+from getvideo.models import Video
 # Create your views here.
+
+@csrf_exempt
+def search_video(request):
+    """
+    A basic search API to search the stored videos using their title and description.
+    """
+    
+    if request.method != 'GET':
+       return JsonResponse({"error":"Method Not Allowed"}, status=405)
+   
+    # get title and description from params
+    title = str(request.GET.get('title',''))
+    description = str(request.GET.get('description',''))
+    
+    #filter out videos containing give title and description    
+    allVideos = Video.objects.filter(title__icontains=title).filter(description__icontains=description).values()
+
+    #return filtered videos
+    return JsonResponse({
+        'video':list(allVideos)
+    })
